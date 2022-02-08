993c1d4abec9b5cd81fff15580dbfa1f22b3e976
sanjipun <iamsanjipun@gmail.com>
2022-02-08 12:47:23 +0545

    add progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Toast from '@components/Toast/Toast';
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <main className="relative" style={{ height: 1000 }}>
       <Toast />
+      <NextNProgress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+        options={{ easing: 'ease', speed: 500, showSpinner: false }}
+      />
       <Component {...pageProps} />
     </main>
   );
