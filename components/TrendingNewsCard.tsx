591706d203c8eec340bf13f2779ee46d06a7c86b
sanjipun <iamsanjipun@gmail.com>
2022-02-14 16:53:13 +0545

    create trending news section

---
+++ components/TrendingNewsCard.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+interface TrendingNewsCardProps {
+  news: {
+    title: string;
+    categoryTag: string;
+    date: string;
+  };
+}
+
+const TrendingNewsCard: React.FC<TrendingNewsCardProps> = ({ news }) => {
+  return (
+    <div className="w-72 text-primary mt-5">
+      <div className="relative h-185 bg-white">
+        <img
+          src="/assets/image.svg"
+          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        />
+        <h1 className="absolute right-2 top-1 px-2 py-1 bg-trendingNewsBg bg-opacity-30 rounded-md text-sm">
+          {news?.categoryTag}
+        </h1>
+      </div>
+      <h1 className="mt-6">{news?.title}</h1>
+      <h3 className="text-sm">{news?.date}</h3>
+    </div>
+  );
+};
+
+export default TrendingNewsCard;
---
+++ views/HomePage/TrendingNews.tsx
@@ -0,0 +1,42 @@
+import TrendingNewsCard from '@components/TrendingNewsCard';
+import React from 'react';
+
+const TrendingNews = () => {
+  return (
+    <div className="bg-trendingNewsBg bg-opacity-20">
+      <section className="py-14 max-w-1440 px-20 mx-auto">
+        <h1 className="text-2xl">Trending News</h1>
+        <div className="flex justify-between flex-wrap">
+          {TRENDING_NEWS.map((news, i) => (
+            <TrendingNewsCard key={i} news={news} />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default TrendingNews;
+
+const TRENDING_NEWS: { title: string; categoryTag: string; date: string }[] = [
+  {
+    title: 'Breakdown:How to Ride the Dip',
+    categoryTag: 'Category Tag',
+    date: 'Feb 12, 2022',
+  },
+  {
+    title: 'Breakdown:How to Ride the Dip',
+    categoryTag: 'Category Tag',
+    date: 'Feb 12, 2022',
+  },
+  {
+    title: 'Breakdown:How to Ride the Dip',
+    categoryTag: 'Category Tag',
+    date: 'Feb 12, 2022',
+  },
+  {
+    title: 'Breakdown:How to Ride the Dip',
+    categoryTag: 'Category Tag',
+    date: 'Feb 12, 2022',
+  },
+];
