69ca2783bca8fc73764da24fea57069a03689549
sanjipun <iamsanjipun@gmail.com>
2022-02-14 16:52:14 +0545

    extend theme in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,26 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./views/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        1440: '1440px'
+      },
+      colors: {
+        primary: '#3C454A',
+        landingBG: '#FAFAFA',
+        trendingNewsBg: '#C4C4C4',
+        footerBg: '#EAECEE'
+      },
+      lineHeight: {
+        56: '56px'
+      },
+      height: {
+        185: '185px'
+      }
+    },
   },
   plugins: [],
 }
