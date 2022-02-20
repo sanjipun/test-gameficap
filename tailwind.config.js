a5b534b5977b6e1a2da7e9132a3962ff109141b8
sanjipun <iamsanjipun@gmail.com>
2022-02-20 09:12:34 +0545

    change variable name in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,20 +7,25 @@ module.exports = {
   theme: {
     extend: {
       maxWidth: {
-        1440: '1440px'
+        1440: '1440px',
+        622: '622px'
       },
       colors: {
         primary: '#3C454A',
-        landingBG: '#FAFAFA',
-        trendingNewsBg: '#C4C4C4',
-        footerBg: '#EAECEE'
+        'FAFAFA': '#FAFAFA',
+        "C4C4C4": '#C4C4C4',
+        "EAECEE": '#EAECEE',
+        "C9D2D8": '#C9D2D8'
       },
       lineHeight: {
         56: '56px'
       },
       height: {
         185: '185px'
-      }
+      },
+      opacity: {
+        15: '0.15'
+      },
     },
   },
   plugins: [],
