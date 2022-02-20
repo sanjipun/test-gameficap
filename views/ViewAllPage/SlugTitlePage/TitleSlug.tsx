1af2af88dee18d7fa5466a71caab5eb6827ba012
sanjipun <iamsanjipun@gmail.com>
2022-02-20 09:20:54 +0545

    add styles to slug title

---
+++ views/ViewAllPage/SlugTitlePage/TitleSlug.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface TitleSlugProps {
+  children: React.ReactNode;
+}
+
+const TitleSlug: React.FC<TitleSlugProps> = ({ children }) => {
+  return <h1 className="text-5xl leading-56 text-primary capitalize">{children}</h1>;
+};
+
+export default TitleSlug;
