7d62675d843b51ad6ce845ef6d31238b098d83e4
sanjipun <iamsanjipun@gmail.com>
2022-03-10 13:53:25 +0545

    add login and signup buttons

---
+++ components/Navbar/Login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface LoginProps {}
+
+const Login: React.FC<LoginProps> = () => {
+  return (
+    <div className="md:mx-2 md:ml-0 mt-3 md:mt-0">
+      <button className="bg-C4C4C4 bg-opacity-20 px-6 py-3 rounded w-full">Login</button>
+    </div>
+  );
+};
+
+export default Login;
---
+++ components/Navbar/SignUp.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface SignUpProps {}
+
+const SignUp: React.FC<SignUpProps> = () => {
+  return (
+    <div className="md:mx-2 md:mr-0 mt-3 md:mt-0">
+      <button className="px-6 py-3 bg-94A2AB rounded w-full">Sign Up</button>
+    </div>
+  );
+};
+
+export default SignUp;
